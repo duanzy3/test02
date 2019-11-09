1da51d5d0161d093c6df0aae7ee7eaf39872d911
duanzy <duanzy3@qq.com>
2019-11-09 11:03:29 +0800

    fib

--- indexjs.js
+++ indexjs.js
@@ -1,35 +1,21 @@
-/*
-    测试天气查询模块
-*/
-const weather = require('./weather.js');/**/
-
-weather.queryWeather('101020100',(data)=>{
-    console.log(data.weatherinfo.WD);
-});
-/*
-    测试天气查询模块
-*/
-const weather = require('./weather.js');/**/
-
-weather.queryWeather('101020100',(data)=>{
-    console.log(data.weatherinfo.WD);
-});
-/*
-    测试天气查询模块
-*/
-const weather = require('./weather.js');/**/
-
-weather.queryWeather('101020100',(data)=>{
-    console.log(data.weatherinfo.WD);
-});
-/*
-    测试天气查询模块
-*/
-const weather = require('./weather.js');/**/
-
-weather.queryWeather('101020100',(data)=>{
-    console.log(data.weatherinfo.WD);
-});
-function(){
-	
+function getRabbitNum(num) {
+	var result = []
+	function fn (n){
+		if (n <= 2){
+			return 1
+		}
+		else {
+			if(result[n]){
+				return result[n];
+			}
+			else {
+				result[n] = arguments.callee(n - 1) + arguments.callee(n -2)
+				return result[n];
+			}
+		}
+	}
+	fn(num)
+	return result;
 }
+
+console.log(getRabbitNum(10))
