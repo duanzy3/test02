d3bde7a8f2e805dfc58e8e51248a198e246aefb7
duanzy <duanzy3@qq.com>
2019-11-09 09:36:12 +0800

    再次提交

--- indexjs.js
+++ indexjs.js
@@ -6,3 +6,11 @@ const weather = require('./weather.js');/**/
 weather.queryWeather('101020100',(data)=>{
     console.log(data.weatherinfo.WD);
 });
+/*
+    测试天气查询模块
+*/
+const weather = require('./weather.js');/**/
+
+weather.queryWeather('101020100',(data)=>{
+    console.log(data.weatherinfo.WD);
+});
